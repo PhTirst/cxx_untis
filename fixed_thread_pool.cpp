3889398f254ebcab57c5b2010db19bdef5e0e66d
PhTirst <54669140+PhTirst@users.noreply.github.com>
2022-05-30 03:30:01 +0800

    Update fixed_thread_pool.cpp

--- fixed_thread_pool.cpp
+++ fixed_thread_pool.cpp
@@ -1,58 +1,72 @@
 #include <mutex>
 #include <queue>
 #include <functional>
-namespace mjx{
-    class fixed_thread_pool{
+
+namespace mjx {
+    class fixed_thread_pool {
     private:
         std::mutex queue_mutex;
         std::condition_variable not_empty_cv;
         std::queue<std::function<void()>> task_queue;
         std::vector<std::thread> threads;
         bool should_shutdown;
-        void thread_main(){
-            for(;;){
-                std::unique_lock<std::mutex> queue_lock(queue_mutex);
-                if(should_shutdown){
-                    break;
-                }else if(task_queue.empty()){
-                    not_empty_cv.wait(queue_lock);
-                }else{
-                    auto task=std::move(task_queue.front());
+
+        void thread_main() {
+            std::unique_lock<std::mutex> queue_lock(queue_mutex);
+            for (;;) {
+                if (!task_queue.empty()) {
+                    auto task = std::move(task_queue.front());
                     task_queue.pop();
                     queue_lock.unlock();
-                    if(task){
+                    if (task) {
                         task();
                     }
+                    queue_lock.lock();
+                } else if (should_shutdown) {
+                    break;
+                } else {
+                    not_empty_cv.wait(queue_lock);
                 }
             }
         }
+
     public:
         explicit fixed_thread_pool(size_t thread_number = std::thread::hardware_concurrency())
-                :should_shutdown(false){
-            if(thread_number>0){
-                threads.reserve(thread_number);
+                : should_shutdown(false) {
+            if (thread_number > 0) {
+                threads.resize(thread_number);
                 for (int i = 0; i < thread_number; ++i) {
-                    threads.emplace_back(std::move(
-                            std::thread([this] { thread_main(); })
-                    ));
+                    threads.emplace_back([this]{thread_main();}).detach();
                 }
             }
         }
-        ~fixed_thread_pool(){
-            for(auto& thread:threads){
-                thread.join();
+
+        ~fixed_thread_pool() {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+            if(!should_shutdown){
+                lock.unlock();
+                stop();
             }
         }
-        void stop(){
-            std::lock_guard<std::mutex> guard(queue_mutex);
-            should_shutdown = true;
-        }
-        void submit_task(std::function<void()> task){
+
+        void stop() {
             {
                 std::lock_guard<std::mutex> guard(queue_mutex);
-                task_queue.push(std::move(task));
+                should_shutdown = true;
             }
             not_empty_cv.notify_all();
+        }
+
+        void submit_task(std::function<void()> task) {
+            {
+                std::lock_guard<std::mutex> guard(queue_mutex);
+                if (should_shutdown) {
+                    return;
+                } else {
+                    task_queue.push(std::move(task));
+                }
+            }
+            not_empty_cv.notify_one();
         }
     };
 }
